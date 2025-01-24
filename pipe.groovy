d74c9ed0a786ee38665b8e7ea4d17e6d8844a125
pushdown99 <haeyun@mgmail.com>
2025-01-24 10:25:25 +0900

    make it better

--- pipe.groovy
+++ pipe.groovy
@@ -10,11 +10,15 @@ spec:
       args:
         - -f
         - /dev/null
+      securityContext:
+        runAsUser: 1000
+        runAsGroup: 1000
+        fsGroup: 1000
 ''') {
     node(POD_LABEL) {
         stage("test") {
             container('java17') {
-                sh "java -version"
+                sh "echo hello"
             }
         }
     }
