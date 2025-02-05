baa3af4046d5c3a5f60a24647061a5c5487ca779
pushdown99 <haeyun@mgmail.com>
2025-02-05 12:03:06 +0900

    make it better

--- index.js
+++ index.js
@@ -4,18 +4,10 @@ const express = require('express');
 const app = express();
 const PORT = env.PORT || 3000;
 
-//////////////////
-//
-// Express Web Service
-//
 app.get('/', (req, res) => {
     res.send('Hello, World 2');
 });
 
-//
-
-///////////////////////////////////
-//
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
